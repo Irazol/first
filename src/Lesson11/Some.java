4442fe01750583d0a67147d1c8648afb2c4fdf0f
Ira <118620892+Irazol@users.noreply.github.com>
2023-01-08 12:12:06 +0200

    Update Some.java

--- src/Lesson11/Some.java
+++ src/Lesson11/Some.java
@@ -3,18 +3,18 @@ package Lesson11;
 public class Some {
     int n = 1219;
 
-    static void binary(int n) {
+    public void binary(int n) {
         int m = n / 2;
         if (m > 0) binary(m);
         System.out.print(n % 2);
     }
 
-    static void phrase(int n) {
+    public void phrase(int n) {
         System.out.print("Число " + n + " в двійковій системі числення: ");
         binary(n);
     }
 
-    static void number_of_signs(int n) {
+    public void number_of_signs(int n) {
         int count = 0;
         int m = n;
         while (m != 0) {
@@ -25,7 +25,7 @@ public class Some {
         System.out.println("Кількість знаків в числі " + n + " : " + count);
     }
 
-    static void min_divisor(int n) {
+    public void min_divisor(int n) {
         int i = 2;
         while (n % i != 0) {
             i++;
